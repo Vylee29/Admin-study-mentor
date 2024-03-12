e549c9a20bc90912c19dffe95093bbdecf6bcdec
huynhson0809 <sonhuynh.08092002@gmail.com>
2024-03-12 22:49:48 +0700

    Revert "[FEATURE] create layout"

--- src/pages/dashboard/DashboardPage.tsx
+++
@@ -1,5 +0,0 @@
-function DashboardPage() {
-  return <div className='w-full'>Dashboard page</div>;
-}
-
-export default DashboardPage;
--- src/routes/route.constant.ts
+++ src/routes/route.constant.ts
@@ -1,10 +1,5 @@
 export const MY_ROUTE = {
   HOME: '',
-  DASHBOARD: '/dashboard',
-  STUDENT: '/student',
-  MENTOR: '/mentor',
-  REPORT: '/report',
-  CHAT: '/chat',
 };
 
 export const DEFAULT_ROUTE = MY_ROUTE.HOME;
